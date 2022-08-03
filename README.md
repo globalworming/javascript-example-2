b478a56ec316d91bc43bfc0715ba8d1c2117d834
globalworming <andreas.worm@headissue.com>
2022-08-03 12:58:03 +0200

    operator examples

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # javascript-example
 
-more exercises
-
 fork and clone this repository, open the index.html in a browser  
-you should after a short loading time see `hello globalworming`  
-edit the file, make the browser display `hello hero` instead, then follow the instructions
+edit the file to make the test pass
--- test.js
+++ test.js
@@ -1,13 +1,33 @@
-const runTest = () => {
-    try {
-        let actual = document.getElementById("app").innerText;
-        let expected = "hello hero";
-        if (actual !== expected) {
-            document.getElementsByClassName("error")[0].textContent = `test failed, expected <${expected}> but was <${actual}>`
-        } else {
-            document.getElementById("displayOnSuccess").hidden = false
-        }
-    } catch (e) {
-        document.getElementsByClassName("error")[0].textContent = e.message + "\n\n" + e.stack
+const RunTest =  {
+    operators1() {
+        setTimeout(() => {
+            try {
+                let text = document.getElementById("willpower").innerText;
+                if (parseInt(text) < 100) {
+                    document.getElementById("displayOnSuccess").hidden = false
+                } else {
+                    document.getElementsByClassName("error")[0].textContent = `test failed, expected less than 100, found ${text}`
+                }
+            } catch (e) {
+                document.getElementsByClassName("error")[0].textContent = e.message + "\n\n" + e.stack
+                throw e
+            }
+        }, 1000)
+    },
+    operators2() {
+        setTimeout(() => {
+            try {
+                let text = document.getElementById("eat").innerText;
+                if (text === "yes") {
+                    document.getElementById("displayOnSuccess").hidden = false
+                } else {
+                    document.getElementsByClassName("error")[0].textContent = `test failed, expected "yes", found ${text}`
+                }
+            } catch (e) {
+                document.getElementsByClassName("error")[0].textContent = e.message + "\n\n" + e.stack
+                throw e
+            }
+        }, 1000)
     }
+
 }
